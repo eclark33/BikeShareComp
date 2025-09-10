5547550eecb4f480bd804f0958369050c0431e27
eclark33 <eliseclark13@gmail.com>
2025-09-10 12:08:59 -0600

    Added R code for a linear regression model and predictions

--- Bike Kaggle Code.R
+++ Bike Kaggle Code.R
@@ -17,6 +17,10 @@ bike_data <- bike_data %>%
 # look at variable types & num of obs
 glimpse(bike_data)
 
+
+
+######## EDA ##########
+
 # bar plot of holiday and working day (good or no?)
 plot_bar(bike_data)
 
@@ -55,6 +59,52 @@ wind_plot <- ggplot(data = bike_data, aes(x = windspeed)) +
   labs(title = "Windspeed and Bike Rentals", x = "Windspeed", y = "Frequency")
 
 (weather_plot + temp_plot) / (humid_plot + wind_plot)
+
+
+# note that test dataset has 1 category 4 while training dataset does not 
+
+
+
+
+###### LINEAR REGRESSION ######
+
+# drop casual & registered from training dataset 
+bike_data <- bike_data %>% select(-casual, -registered)
+
+# read in test data
+testData <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/BikeShareComp/bike-sharing-demand/test.csv")
+
+# set factors in test data 
+testData <- testData %>%
+  mutate(weather = as.factor(weather), 
+         season = as.factor(season),
+         workingday = as.factor(workingday), 
+         holiday = as.factor(holiday))
+
+# linear model
+my_linear_model <- linear_reg() %>% #Type of model
+  set_engine("lm") %>% # Engine = What R function to use
+  set_mode("regression") %>% # Regression just means quantitative response
+  fit(formula = count ~ . - datetime, data = bike_data)
+
+
+# predictions
+bike_predictions <- predict(my_linear_model, new_data = testData)
+
+
+kaggle_submission <- bike_predictions %>%
+  bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count = .pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count = pmax(0, count)) %>% # pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+# write up file for kaggle
+vroom_write(x = kaggle_submission, file = "./LinearPreds.csv", delim=",")
+
+
+
+
 
 
 
